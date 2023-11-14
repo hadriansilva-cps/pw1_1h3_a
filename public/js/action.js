49ef65b7f569b1462253b688fe9150ea48ca7395
hadriansilva-cps <hadrian.silva01@etec.sp.gov.br>
2023-11-14 20:48:10 -0300

    aula1411

--- public/js/action.js
+++ public/js/action.js
@@ -1,14 +1,29 @@
-function somaNumero(n1, n2, op){
 
-    let resultado=0;
-
-    if(op==='c'){
-        resultado=(n1+n2)
-        console.table(resultado)
-    }
-    if(op==='a'){
-        resultado=(n1 - n2)
-        alert(resultado)
-    }
-
+function calculanumeros(){
+  let n1 = parseInt(document.getElementById("usuario").value)
+  let n2 = parseInt(document.getElementById("senha").value)
+  let op = document.getElementById("operation").value
+  let resultado
+  if (op === "+") {
+    resultado = (n1+n2)
+    alert("Soma: " +n1+" "+op+" "+n2+ " = " +resultado)
+  }
+  else if(op === "-"){
+    resultado = (n1-n2)
+    alert("subtração: " +n1+" "+op+" "+n2+ " = " +resultado)
+  }
+  else if(op === "*"){
+    resultado = (n1*n2)
+    alert("multiplicação: " +n1+" "+op+" "+n2+ " = " +resultado)
+  }
+  else if(op === "/"){
+    resultado = (n1/n2)
+    alert("divisão: " +n1+" "+op+" "+n2+ " = " +resultado)
+  }
+  else if(op === "sqrt"){
+    resultado = Math.sqrt(n1+n2)
+    alert("raiz" +resultado)
+  }
+  
 }
+
