d35eb0844ce4a4572f09c4133f84c3ef73b6e144
hadriansilva-cps <hadrian.silva01@etec.sp.gov.br>
2023-11-21 20:42:57 -0300

    aula2111 modulos em javascript

---
+++ public/js/ModuloDados.js
@@ -0,0 +1,7 @@
+const userlogin = [
+    {id: 1001,username: "victoicoma", senha:"gordinho123",email:"victorsena301@gmail.com"},
+    {id: 1002,username: "marcelofarofa", senha:"123456", email: "mfarofa@gmail.com"}
+]
+export default userlogin 
+
+
--- public/js/action.js
+++ public/js/action.js
@@ -1,29 +1,12 @@
+import userlogin from "./ModuloDados.js";
 
-function calculanumeros(){
-  let n1 = parseInt(document.getElementById("usuario").value)
-  let n2 = parseInt(document.getElementById("senha").value)
-  let op = document.getElementById("operation").value
-  let resultado
-  if (op === "+") {
-    resultado = (n1+n2)
-    alert("Soma: " +n1+" "+op+" "+n2+ " = " +resultado)
-  }
-  else if(op === "-"){
-    resultado = (n1-n2)
-    alert("subtração: " +n1+" "+op+" "+n2+ " = " +resultado)
-  }
-  else if(op === "*"){
-    resultado = (n1*n2)
-    alert("multiplicação: " +n1+" "+op+" "+n2+ " = " +resultado)
-  }
-  else if(op === "/"){
-    resultado = (n1/n2)
-    alert("divisão: " +n1+" "+op+" "+n2+ " = " +resultado)
-  }
-  else if(op === "sqrt"){
-    resultado = Math.sqrt(n1+n2)
-    alert("raiz" +resultado)
-  }
-  
-}
+const login = document.getElementById("login")
+
+login.addEventListener("click", (e) => {
+
+  let user = document.getElementById("usuario").value
+  let senha = document.getElementById("senha").value
+  console.table(user, senha)
+  console.table(userlogin)
+})
 
