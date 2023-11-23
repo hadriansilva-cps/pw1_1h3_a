a02b4607e184a996dd5a0b04d45caf9646a59b20
hadriansilva-cps <hadrian.silva01@etec.sp.gov.br>
2023-11-22 22:38:58 -0300

    aula2211 Arrays e Interações

--- public/js/action.js
+++ public/js/action.js
@@ -1,12 +1,20 @@
 import userlogin from "./ModuloDados.js";
 
 const login = document.getElementById("login")
+const cadastrar = document.getElementById("cadastro")
 
 login.addEventListener("click", (e) => {
 
   let user = document.getElementById("usuario").value
   let senha = document.getElementById("senha").value
-  console.table(user, senha)
-  console.table(userlogin)
+
+ 
 })
 
+cadastrar.addEventListener("click", (e) => {
+  var usuario = userlogin.find((userlogin) => userlogin.email === "victorsena301@gmail.com")
+  console.log(usuario)
+})
+function encontraUsuario(value, index, array){
+  console.table(value.email)
+}
